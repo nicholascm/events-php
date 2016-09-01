74fb3571e0f43879aa9dd082f59c50c9c4f09fd1
nicholascm90 <nicholascm90@gmail.com>
2016-09-01 06:09:33 -0400

    renaming event_users

--- app/EventUser.php
+++ app/EventUser.php
@@ -6,8 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class EventUser extends Model
 {
-  protected $table = 'event_user';
-  
+  protected $table = 'event_users';
+
   protected $fillable = [
       'user_id', 'event_id'
   ];
